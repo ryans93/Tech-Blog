6aad53893ff5c412e5bd88e8ac917eb4dba36a56
Ryan Strickler <ryanstrickler@Ryans-MacBook-Pro.local>
2025-04-19 18:29:26 -0400

    login/signup styling

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,7 +2,20 @@ let cards = document.getElementsByClassName("card");
 
 for (card of cards){
     card.addEventListener("click", function (event) {
-        let element = event.target.matches(".card") ? event.target : event.target.matches(".container") ? event.target.parentElement : event.target.parentElement.parentElement;
+        let element = ""
+        if (event.target.matches(".card")){
+            element = event.target;
+        }
+        else if (event.target.matches(".container")){
+            element = event.target.parentElement;
+        }
+        else if (event.target.matches(".card-header-container") || event.target.matches("p")){
+            element = event.target.parentElement.parentElement;
+        }
+        else {
+            element = event.target.parentElement.parentElement.parentElement;
+        }
+        
         console.log(element.getAttribute("data-id"))
         window.location.href = `/post/${element.getAttribute("data-id")}`
     })
